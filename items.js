c76136b86831139444da646343590aff5755930c
Ymurzio <ymurzio@pm.me>
2025-11-15 12:43:28 -0500

    Add Box Drop Down to Items Adding the Box Drop Down on the Items page.
    Drop down is populated with the name of the box that items has. Otherwise it is blank.
    When the drop down is changed the box is updated via the API.

--- items.js
+++ items.js
@@ -3,6 +3,7 @@ const dataIndex = 0;
 const idIndex = 1;
 const boxIndex = 2;
 
+var boxes = [];
 //deleteItem('http://localhost:8080/item/3');
 
 createItemsTable();
@@ -45,6 +46,7 @@ async function getItems() {
 
 async function createItemsTable() {
     await getItems();
+    await getBoxes();
 
     // create header
     var myDiv = document.getElementById("itemstablediv");
@@ -54,7 +56,13 @@ async function createItemsTable() {
     var header = document.createElement("tr");
     var itemName = document.createElement("th");
     itemName.appendChild(document.createTextNode("Items"));
+    var deleteColumn = document.createElement("th");
+    var boxColumn = document.createElement("th");
+    boxColumn.appendChild(document.createTextNode("Box"));
+
     header.appendChild(itemName);
+    header.appendChild(deleteColumn);
+    header.appendChild(boxColumn);
     table.appendChild(header);
     
     // create rows
@@ -82,14 +90,84 @@ async function createItemsTable() {
 
         deleteTD.appendChild(del);
 
+        // box name here
+        var boxTD = document.createElement("td");
+
+        var boxDropdown = document.createElement("select");
+
+        var defaultCreated = false;
+        var boxId = await fetchBoxId(items[i][boxIndex]);
+        console.log("box url for default:", boxId);
+        // create options for dropdown with the names of the boxes
+        for (var j = 0; j < boxes.length; j++) {
+            var boxOption = document.createElement("option");
+            boxOption.value = boxes[j][idIndex];
+            boxOption.textContent = boxes[j][dataIndex];
+
+            if (boxId === boxes[j][idIndex]) {
+                boxOption.selected = true;
+                console.log("HI FROM DEFAULT DD");
+                defaultCreated = true;
+            }
+
+            boxDropdown.appendChild(boxOption);   
+
+        }
+
+        //create blank option
+        var boxOption = document.createElement("option");
+        if (defaultCreated === false) {
+            boxOption.selected = true;
+        }
+        boxDropdown.appendChild(boxOption);
+        
+        boxDropdown.setAttribute('item-url', items[i][boxIndex]);
+        boxDropdown.onchange = async function() {
+            await updateBox(this.getAttribute("item-url"), this.value);
+/*            var myDiv = document.getElementById("itemstablediv");
+            var table = document.querySelector("table");
+            myDiv.removeChild(table);
+            items = [];
+            boxes = [];
+            createItemsTable();
+            */
+        };
+
+        boxTD.appendChild(boxDropdown);
+
+
         row.appendChild(rowName);
         row.appendChild(deleteTD);
+        row.appendChild(boxTD);
         table.appendChild(row);
     }
 
     // append the header and rows
     myDiv.appendChild(table);
 
+}
+
+async function updateBox(itemURL, boxURL) {
+    console.log("hi from updateBox");
+    console.log(itemURL);
+    console.log(boxURL);
+
+    await fetch(itemURL, {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'text/uri-list'
+        },
+        body: boxURL
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('box update successfull:', data);
+    })
+    .catch(error => {
+        console.error('error box update:', error);
+    });
+
+    console.log("bye from updateBox");
 }
 
 async function deleteItem(itemURL) {
@@ -109,3 +187,55 @@ async function deleteItem(itemURL) {
 // populate box name that is linked to item
 // drop down with list of boxes previously selected
 // > this will call a put that updates the box then refreshes the table
+
+
+async function getBoxes() {
+    try {
+        var hasNextLink = false;
+        var url = 'http://localhost:8080/box';
+        var j = 0;
+        do {
+            const response = await fetch(url);
+            const myJson = await response.json();
+            console.log(myJson);
+            console.log(myJson._embedded.box.length);
+            for (var i = 0; i < myJson._embedded.box.length; i++) {
+                boxes[j] = [];
+                boxes[j][dataIndex] = myJson._embedded.box[i].name;
+                boxes[j][idIndex] = myJson._embedded.box[i]._links.self.href;
+                boxes[j][boxIndex] = myJson._embedded.box[i]._links.location.href;
+                j++;
+            }
+            try {
+                if (typeof myJson._links.next !== 'undefined') {
+                    hasNextLink = true;
+                    url = myJson._links.next.href;
+                } else {
+                    hasNextLink = false;
+                }
+            } catch(error) {// haven't seen this happen yet
+                console.log('no next link. exiting while', error);
+                hasNextLink = false;
+            }
+        } while (hasNextLink === true);
+    } catch(error) {
+        console.log('no good. bad fetch:', error);
+    }
+    console.log('full arrary', boxes.length);
+}
+
+async function fetchBoxId(itemBoxURL) {
+    var retURL;
+    await fetch(itemBoxURL)
+    .then(response => response.json())
+    .then(data => {
+        console.log('fetch successfull:', data);
+        retURL = data._links.self.href;
+    })
+    .catch(error => {
+        console.error('error fetching:', error);
+    });
+
+    return retURL;
+
+}
